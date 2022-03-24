9bb36f6812df9c54b0ea4123b892be0196e61257
daniel Velazquez <daniel_velazquez96@outlook.es>
2022-03-24 14:17:49 -0600

    Se implemento Styled-components a ShoCards

--- src/components/ShowCards/Card.js
+++ src/components/ShowCards/Card.js
@@ -1,15 +1,46 @@
+import styled from 'styled-components'
 
-import './Card.css'
-
+//styles
+const CardContainer=styled.div`
+    position: relative;
+    border: solid black 2px;
+    height: 7cm;
+`
+const CardImage=styled.img`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    object-fit: fill;
+`
+const CardTitle=styled.div`
+    max-width: 4.9cm;
+    max-height: 50cm;
+    overflow: hidden;
+    position: absolute;
+    top: 78%;
+    left: 50%;
+    transform: translate(-50%,0);
+    text-align: center;
+    line-height: .8cm;
+    font-size: 30px;
+    font-family: 'Roboto', sans-serif;
+`
+const CardNumber=styled.div`
+    position: absolute;
+    top:.1cm;
+    left: .15cm;
+    font-family: 'Lobster', cursive;
+    font-size: 20px;
+`
+// Components
 const Card=({name,url,number})=>{
     return(
-        <div className="showContainer-card">
-                <img src={url} alt="img" />
-                <h2 >{name}</h2>
-                <span>{number}</span>
-        </div>
-    )
-    
+        <CardContainer>
+            <CardImage src={url} alt="img" />
+            <CardTitle>{name}</CardTitle>
+            <CardNumber>{number}</CardNumber>
+        </CardContainer>
+    ) 
 }
 
 export default Card
--- src/components/ShowCards/ShowCards.js
+++ src/components/ShowCards/ShowCards.js
@@ -1,25 +1,33 @@
-import './ShowCards.css'
-
-//Components
+import styled from 'styled-components'
 import Card from './Card.js'
 
-// styles argumnets is optional
+const ShowCardsContainer=styled.div`
+     margin: 50px auto;
+    height: auto;
+    width: 88.5vw;
+    border: solid black 2px;
+    box-shadow: 3px 3px 5px black, 
+    -3px -3px 5px black;
+    display: grid;
+    grid-template-columns: repeat(auto-fill,minmax(4cm,1fr)) ;
+    grid-template-rows: repeat(autofit,minmax(6cm,6.1cm));
+    background-color: white;
+    gap: .3cm;
+`
 const ShowCards=({cards},styles)=>{
     return(
-        <div className="ShowContainer" style={styles} >
-            {
-                cards.length
-                    ?cards.map((card,key)=>{
-                        return<Card
-                                key={key}
-                                name={card.name}
-                                url={card.url}
-                                number={card.number}
-                                />
-                    })
-                    :null
-            }
-        </div>
+        <ShowCardsContainer style={styles} >
+            {cards.length
+                ?cards.map((card,key)=>{
+                    return<Card
+                            key={key}
+                            name={card.name}
+                            url={card.url}
+                            number={card.number}
+                            />
+                })
+                :null}
+        </ShowCardsContainer>
     )
 
 }
